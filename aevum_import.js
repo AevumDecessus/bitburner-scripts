7f0b0f340ebb89cd0d878f176f0d2770339ace23
AevumDecessus <aevum@decess.us>
2021-01-24 16:14:59 +0000

    Added initial import script

---
+++ aevum_import.js
@@ -0,0 +1,40 @@
+let config = {
+  folder: 'aevum',
+  rootUrl: 'https://raw.githubusercontent.com/AevumDecessus/bitburner-scripts/main/',
+  serverPrefix: 'AevumPwns',
+};
+/* 
+ * Imports all files from github $rootUrl, stores them in $folder
+ */
+export async function main(ns) {
+  let filesImported = await importFiles(ns);
+  if (filesImported) {
+    ns.tprint('Successfully Imported Files');
+    await ns.wget(`${config.rootUrl}aevum_import.js`, 'aevum_import.js');
+  } else {
+    ns.tprint('Something failed, check files and paths');
+  }
+}
+
+async function importFiles(ns) {
+  let files = [
+    'aevumGrow.js',
+    'aevumHack.js',
+    'aevumWeaken.js'
+      ];
+  let filesImported = true;
+  for (let file of files) {
+    let remoteFileName = `${config.rootUrl}scripts/${file}`;
+    let result = ns.wget(remoteFileName, `/{getFolder()}/${file}`);
+    filesImported = filesImported && result;
+    ns.tprint(`File: ${file}: ${result ? '✔️' : '❌'}`);
+  }
+  return filesImported;
+}
+
+export function getFolder() {
+  return config.folder;
+}
+export function getServerPrefix() {
+  return config.serverPrefix;
+}
