a316a0494e53aff2f5e717bcdb3f53e1777eb581
AevumDecessus <aevum@decess.us>
2021-01-24 16:24:58 +0000

    Added missing await

--- aevum_import.js
+++ aevum_import.js
@@ -25,7 +25,7 @@ async function importFiles(ns) {
   let filesImported = true;
   for (let file of files) {
     let remoteFileName = `${config.rootUrl}scripts/${file}`;
-    let result = ns.wget(remoteFileName, `/{getFolder()}/${file}`);
+    let result = await ns.wget(remoteFileName, `/{getFolder()}/${file}`);
     filesImported = filesImported && result;
     ns.tprint(`File: ${file}: ${result ? '✔️' : '❌'}`);
   }
