b4f741b618c304c0f8af4aabff3e2e653be410cd
AevumDecessus <aevum@decess.us>
2021-01-31 17:47:56 +0000

    Don't target servers we can't hack

--- scripts/targeter.js
+++ scripts/targeter.js
@@ -5,13 +5,15 @@ export async function main(ns) {
   let servers = await ns.read('allServers.txt');
   servers = servers.split(';');
   var doLoop = true;
+  var hackSkill = 0;
 
   var lastTarget = [];
   while (doLoop) {
+    hackSkill = await ns.getHackingLevel();
     for (var i = 0; i < servers.length; i++) {
       var server = servers[i].split(',');
       // Only consider targets we have root on first
-      if (await ns.hasRootAccess(server[0])) {
+      if ((await ns.hasRootAccess(server[0])) && (await ns.getServerRequiredHackingLevel(server[0]) <= hackSkill)) {
         var target = server[0];
         var hasRun = false;
         var shouldSwitchTargets = false;
